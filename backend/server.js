a2f60f7dccb29e19b37203396b012cf669133d02
Alok Patel <alokpatel41001@gmail.com>
2025-06-15 17:39:41 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -20,16 +20,10 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 // Deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
+
   app.get("/", (req, res) => {
     res.send("API is running..");
-  });
-}
+  })
 
 // Error Handling Middleware
 app.use(notFound);
